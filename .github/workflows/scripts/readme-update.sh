a1347829e32b0a087d19dcb46f5b8051dfa81bb6
Vladimir Ischenko <vladimir.ischenko@onlyoffice.com>
2025-04-17 14:45:02 +0300

    Create readme-update.sh

---
+++ .github/workflows/scripts/readme-update.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -e
+WORKFLOW_FILE=".github/workflows/ci-update.yml"
+README_FILE="README.md"
+if ! command -v yq &> /dev/null; then
+  sudo wget -q https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O /usr/local/bin/yq
+  sudo chmod +x /usr/local/bin/yq
+fi
+OS_LIST=$(yq -r '.jobs["update-test"].strategy.matrix.include[].name' "$WORKFLOW_FILE")
+NEW_OS_BLOCK=""
+while IFS= read -r os; do
+  [ -n "$os" ] && NEW_OS_BLOCK="$NEW_OS_BLOCK- $os"$'\n'
+done <<< "$OS_LIST"
+TMPFILE=$(mktemp)
+printf '%s\n' "<!-- OS-SUPPORT-LIST-START -->" "$NEW_OS_BLOCK" "<!-- OS-SUPPORT-LIST-END -->" > "$TMPFILE"
+sed -i '/<!-- OS-SUPPORT-LIST-START -->/,/<!-- OS-SUPPORT-LIST-END -->/{
+r '"$TMPFILE"'
+d
+}' "$README_FILE"
+rm "$TMPFILE"
+
